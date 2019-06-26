88bb5767941c774cf788f8fa1ccb427b1260c3e6
zhangwenjie <1084923301@qq.com>
2019-06-26 19:56:56 +0800

    hello

---
+++ demo02/src/cn/itcast/test/demotest.java
@@ -0,0 +1,13 @@
+package cn.itcast.test;
+
+import java.sql.SQLOutput;
+
+public class demotest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
