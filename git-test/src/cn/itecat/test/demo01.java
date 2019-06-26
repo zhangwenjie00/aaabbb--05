1132c14cded3286ec711b0887437e94080be3282
zhangwenjie <1084923301@qq.com>
2019-06-26 19:52:29 +0800

    hello word

---
+++ git-test/src/cn/itecat/test/demo01.java
@@ -0,0 +1,15 @@
+package cn.itecat.test;
+
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello,world!!!");
+        System.out.println("hello,world!!!");
+        System.out.println("hello,world!!!");
+        System.out.println("hello,world!!!");
+        System.out.println("hello,world!!!");
+    }
+
+    public void  test(){
+        System.out.println("a=10");
+    }
+}
